f10ad09466bcc9727a7baa150733f56d791a1a2c
Dibyendu Mukherjee <dibyendumukherjee@Dibyendus-MacBook-Pro.local>
2018-05-28 12:25:50 -0400

    Using chalk to make the CLI text ligible in dark screen

--- bin/helpers/read-cli.js
+++ bin/helpers/read-cli.js
@@ -1,33 +1,34 @@
 const yargsInteractive = require('yargs-interactive')
+const chalk = require('chalk')
 const Joi = require('joi')
 const options = {
   interactive: { default: true },
   name: {
     type: 'input',
     default: '',
-    describe: 'Enter app name (required field, defaults to sample-app):'
+    describe: chalk.bgBlackBright(chalk.greenBright('Enter app name (required field, defaults to sample-app):'))
   },
   author: {
     type: 'input',
     default: '',
-    describe: 'Enter author\'s name:'
+    describe: chalk.bgBlackBright(chalk.greenBright('Enter author\'s name:'))
   },
   description: {
     type: 'input',
     default: '',
-    describe: 'Enter project description'
+    describe: chalk.bgBlackBright(chalk.greenBright('Enter project description'))
   },
   license: {
     type: 'input',
     default: '',
-    describe: 'Enter license'
+    describe: chalk.bgBlackBright(chalk.greenBright('Enter license'))
   }
 }
 
 const readCli = () => {
    return yargsInteractive()
     .usage('$0 <command> [args]')
-    .interactive(options)
+    .interactive(options)      
     .then((result)=>{
       if(!validInput(result.name)) result.name = 'sample-app'
       return result
